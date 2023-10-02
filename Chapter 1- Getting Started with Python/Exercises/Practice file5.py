d2443e91f03b4fba0dd4a6eb4fd222acbd26be2a
Kamrankn <kamrannadeemkn5@gmail.com>
2023-10-02 11:48:36 +0400

    Practice file5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice file5.py
@@ -0,0 +1,11 @@
+# Define four different strings in separate variables
+string1 = "The"
+string2 = " Cricket worldcup"
+string3 = " is starting"
+string4 = " this October."
+
+# Concatenate the strings and store the result in a new variable
+result = string1 + string2 + string3 + string4
+
+# Print the concatenated string
+print(result)
