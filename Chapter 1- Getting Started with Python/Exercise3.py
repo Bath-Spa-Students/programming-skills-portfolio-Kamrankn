67404405b98eb7145e6391d9fb3cc4e5a1ef7e29
Kamrankn <kamrannadeemkn5@gmail.com>
2023-10-02 09:02:42 +0400

    Exercise 3.py

---
+++ Chapter 1- Getting Started with Python/Exercise3.py
@@ -0,0 +1,10 @@
+import datetime
+
+# Get the current date and time
+current_datetime = datetime.datetime.now()
+
+# Format the date and time as a string
+formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+# Print the current date and time
+print("Current Date and Time:", formatted_datetime)
