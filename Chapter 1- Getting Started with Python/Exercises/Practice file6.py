8b83280042422bb6a873e28434ac843851ffd015
Kamrankn <kamrannadeemkn5@gmail.com>
2023-10-05 12:26:02 +0400

    Practice file.6

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice file6.py
@@ -0,0 +1,8 @@
+name = "Kamran"
+age = 18
+Status = "Studying at bathspa"
+hobbies = ["Cricket", "Football", "Gaming"]
+
+# Generate and print the bio
+character_bio = (name, age, Status, hobbies)
+print(character_bio)
