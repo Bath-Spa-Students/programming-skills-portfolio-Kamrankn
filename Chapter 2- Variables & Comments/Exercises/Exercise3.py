b3953d660c7452f0f62f54966443ae17b492fd35
Kamrankn <kamrannadeemkn5@gmail.com>
2023-10-12 12:29:00 +0400

    Exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,11 @@
+# Define a variable with whitespace characters
+name_with_whitespace = "\t \n Kamran \t \n"
+
+print(name_with_whitespace)
+
+print("Using lstrip():", name_with_whitespace.lstrip())
+
+print("Using rstrip():", name_with_whitespace.rstrip())
+
+# Use strip() to remove both leading and trailing whitespace
+print("Using strip():", name_with_whitespace.strip())
