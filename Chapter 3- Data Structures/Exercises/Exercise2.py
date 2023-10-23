5369cb27fbb3ecd7e2479c8b3d077a8f8df31f65
Kamrankn <kamrannadeemkn5@gmail.com>
2023-10-23 11:55:11 +0400

    Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,7 @@
+# Create a list of names
+names = ["Taha", "Basim", "Umer", "Rafay"]
+
+# Print a personalized message to each person
+for name in names:
+    message = f"Hello, {name}! I hope you're having a great day."
+    print(message)
