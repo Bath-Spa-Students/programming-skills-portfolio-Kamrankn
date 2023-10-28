6cc4f13b1decde545dc2d59c235567ddeee74a67
Kamrankn <kamrannadeemkn5@gmail.com>
2023-10-28 15:21:43 +0400

    Exercise4.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise4.py
@@ -0,0 +1,19 @@
+rivers = {
+    'nile': 'Egypt',
+    'amazon': 'Brazil',
+    'Indus': 'Pakistan'
+}
+
+# Print a sentence about each river
+for river, country in rivers.items():
+    print(f"The {river.title()} runs through {country}.")
+
+# Print the name of each river
+print("\nRivers:")
+for river in rivers.keys():
+    print(river.title())
+
+# Print the name of each country
+print("\nCountries:")
+for country in rivers.values():
+    print(country)
