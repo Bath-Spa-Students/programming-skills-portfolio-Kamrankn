82ee9c9b6dd481dcc4b01fab23eb397a2c8674fd
Kamrankn <kamrannadeemkn5@gmail.com>
2023-10-28 14:37:07 +0400

    Exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,9 @@
+alien_color = 'green'  # Alien's color is green
+
+if alien_color == 'green':
+    print("Player just earned 5 points.")
+
+alien_color = 'yellow'  # Alien's color is yellow
+
+if alien_color == 'green':
+    print("Player just earned 5 points.")
