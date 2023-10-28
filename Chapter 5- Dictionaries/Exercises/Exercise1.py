2b0d4a9c9063124db6ad5221c39410e239c1fca0
Kamrankn <kamrannadeemkn5@gmail.com>
2023-10-28 15:08:28 +0400

    Exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,12 @@
+person_info = {
+    "first_name": "Rafay",
+    "last_name": "Shah",
+    "age": 20,
+    "city": "Karachi"
+}
+
+# Printing each piece of information from the dictionary
+print("First Name: " + person_info["first_name"])
+print("Last Name: " + person_info["last_name"])
+print("Age: " + str(person_info["age"]))
+print("City: " + person_info["city"])
