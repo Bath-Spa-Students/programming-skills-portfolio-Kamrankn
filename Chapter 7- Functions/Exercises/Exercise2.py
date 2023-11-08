51a61027aadc2683521af40cb51d11415f0d9e6d
Kamrankn <kamrannadeemkn5@gmail.com>
2023-11-09 01:34:07 +0400

    Exercise2.py

---
+++ Chapter 7- Functions/Exercises/Exercise2.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+# Call the function and pass your favorite book title as an argument
+favorite_book("A Tale of two Cities")
