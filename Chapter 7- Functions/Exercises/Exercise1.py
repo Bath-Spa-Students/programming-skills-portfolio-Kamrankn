e450fc88a23d966777cbaebbb3f6ea7d4bb13af6
Kamrankn <kamrannadeemkn5@gmail.com>
2023-11-09 01:30:17 +0400

    Exercise1.py

---
+++ Chapter 7- Functions/Exercises/Exercise1.py
@@ -0,0 +1,6 @@
+def display_message():
+    chapter_topic = "Python functions."
+    print(f"In this chapter, I am learning about {chapter_topic}")
+
+# Call the function to display the message
+display_message()
