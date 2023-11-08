d1b4d8c886e60ab2f325daa9f187b4bbc713e0cb
Kamrankn <kamrannadeemkn5@gmail.com>
2023-11-09 01:42:00 +0400

    Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,8 @@
+def make_shirt(size, message):
+    print(f"Creating a {size}-sized shirt with the message: {message}")
+
+# Call the function using positional arguments
+make_shirt("Medium", "Born To Lead!")
+
+# Call the function using keyword arguments
+make_shirt(size="Large", message="The best is yet to be!")
