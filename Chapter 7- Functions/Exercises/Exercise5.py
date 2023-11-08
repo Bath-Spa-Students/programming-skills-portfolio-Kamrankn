489717accf91ce432e320d654b52d6af0d47bbec
Kamrankn <kamrannadeemkn5@gmail.com>
2023-11-09 01:53:47 +0400

    Exercise5.py

---
+++ Chapter 7- Functions/Exercises/Exercise5.py
@@ -0,0 +1,7 @@
+def describe_city(city, country="Unknown"):
+    print(f"{city} is in {country}.")
+
+# Call the function for three different cities
+describe_city("Canberra", "Australia")
+describe_city("Berlin", "Germany")
+describe_city("Ottawa")
