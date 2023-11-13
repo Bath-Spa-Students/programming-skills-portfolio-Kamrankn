e6e06daf1adf0fd99cf5453fb66b22048b61fe03
Kamrankn <kamrannadeemkn5@gmail.com>
2023-11-13 19:55:58 +0400

    Practicefile1.py

--- Chapter 4- Control Flow/Exercises/Practicefile1.py
+++ Chapter 4- Control Flow/Exercises/Practicefile1.py
@@ -1,13 +1,13 @@
 # Assuming the variable & has a value
-A = 120  
+A = 105
 
 if A > 100:
     y = 20
     z = 40
 else:
     # Assign default values when condition is not met
-    y = None
-    z = None
+    y = 10
+    z = 25
 
 print("y:", y)
 print("z:", z)
